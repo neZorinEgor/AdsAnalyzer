61d4ce82383523c66973ef2795f878cbb51ebcf7
NeZorinEgor <zorin-ep@mail.ru>
2024-10-05 00:51:18 +0800

    feat: add 2 field at user model

--- src/handlers/model.py
+++ src/handlers/model.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class ClassificationHandlersModel(Base):
-    __tablename__ = "classification_handler"
+    __tablename__ = "handler"
 
-    handler_id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     endpoint_path: Mapped[str_128] = mapped_column(nullable=False)
     model_path: Mapped[str_128] = mapped_column(nullable=False)
     created_at: Mapped[datetime.datetime] = mapped_column(nullable=False, default=datetime.datetime.now(datetime.UTC))
